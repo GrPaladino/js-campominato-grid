923bf211ff733f3a1f7545cb6f4b47980961e9b4
GrPaladino <paladino.graziano@gmail.com>
2024-01-18 13:43:10 +0100

    first commit

---
+++ Js/main.js
@@ -0,0 +1,5 @@
+// - creo gli elementi in html e i loro stili in css
+// - richiamo gli elementi da caricare al caricamento della pagina
+// - alla pressione del tasto dovrò generare una griglia con le celle
+// - al click sulla cella, la cella si colora di azzurro e stampo in console il numero della cella cliccata
+// - applico gli eventuali bonus
